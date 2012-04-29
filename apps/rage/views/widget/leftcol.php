331536cc6cec1e787e3e25a439a929b548e8cbb3
Jinggo Villamor <jingcleovil@gmail.com>
2012-04-29 18:54:57 +0800

    Fixed update

--- apps/rage/views/widget/leftcol.php
+++ apps/rage/views/widget/leftcol.php
@@ -11,15 +11,18 @@
 			<li class="nav-header">Account</li>
 			<li class="<?=$page == "stream" ? 'active' : ''?>"><a href="/"><i class="<?=$page == "stream" ? 'icon-white' : ''?>  icon-list-alt"></i> Stream</a></li>
 			<li class="<?=$page == "character" ? 'active' : ''?>"><a href="/characters"><i class="<?=$page == "character" ? 'icon-white' : ''?> icon-user"></i> Characters</a></li>
+			<!--
 			<li class="<?=$page == "storage" ? 'active' : ''?>"><a href="/characters/storage"><i class="<?=$page == "storage" ? 'icon-white' : ''?> icon-inbox"></i> Storage</a></li>
+			-->
 			<li class="<?=$page == "settings" ? 'active' : ''?>"><a href="/account/settings"><i class="<?=$page == "settings" ? 'icon-white' : ''?> icon-cog"></i> Settings</a></li>
-			
 			<li class="divider"></li>
 			
 			<li class="nav-header">Support</li>
-			<li class="<?=$page == "help-guide" ? 'active' : ''?>"><a href="/help-guide"><i class="<?=$page == "help-guide" ? 'icon-white' : ''?> icon-question-sign"></i> Help Guide</a></li>
-			<li><a href="/ticket/file"><i class="icon-file"></i> File a Ticket</a></li>
 			
+			<li class="<?=$page == "help-guide" ? 'active' : ''?>"><a href="/help-guide"><i class="<?=$page == "help-guide" ? 'icon-white' : ''?> icon-question-sign"></i> Help Guide</a></li>
+			<!--
+			<li><a href="/ticket/file"><i class="icon-file"></i> File a Ticket</a></li>
+			-->
 			<li class="divider"></li>
 			
 			<?php if(isset($isAdmin) && $isAdmin) { ?> 
