a4d434b8fd23ebd0ab041899f9d4369c432d4279
Jinggo Villamor <jingcleovil@gmail.com>
2012-04-29 14:47:48 +0800

    Hide is admin

--- apps/rage/views/widget/leftcol.php
+++ apps/rage/views/widget/leftcol.php
@@ -25,8 +25,9 @@
 			<?php if(isset($isAdmin) && $isAdmin) { ?> 
 			<li class="nav-header">Admin</li>
 			<li class="<?=$page == "account" ? 'active' : ''?>"><a href="/account"><i class="<?=$page == "account" ? 'icon-white' : ''?> icon-question-sign"></i> Accounts</a></li>
-			<? } ?>
 			<li class="divider"></li>
+			<? } ?>
+			
 			
 			<li><a href="/account/signout"><i class="icon-off"></i> Logout</a></li>
         </ul>
