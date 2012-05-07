5c0a412ada3f8c53ae85ae96cb599bc50182b8b0
Jinggo <jingcleovil@gmail.com>
2012-05-08 01:41:31 +0800

    Added downloads

--- apps/rage/views/main/index.php
+++ apps/rage/views/main/index.php
@@ -61,7 +61,7 @@
 			<div class="features clearfix">
 		
 				<div class="features_details">
-					<a href="http://www.mediafire.com/download.php?h20xbbbgummmtd8" target="_blank" class="btn btn-large">Download (v1.1-Lion)</a>
+					<a href="http://www.mediafire.com/download.php?ohlxpxglegwebqb" target="_blank" class="btn btn-large">Download (v1.2-Lion)</a>
 				</div>
 			</div>
 		
