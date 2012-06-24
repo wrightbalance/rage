6c1be0371daaf1b4d098c9116a75e599043751d9
Jinggo Villamor <jingcleovil@gmail.com>
2012-06-24 21:01:51 +0800

    CHanges on database side

--- apps/config/database.php
+++ apps/config/database.php
@@ -3,6 +3,24 @@
 $active_group = 'server';
 $active_record = TRUE;
 
+if(strpos(config_item('base_url'),'ragnagears.com')) $active_group = "demo";
+
+$db['demo']['hostname'] = 'localhost';
+$db['demo']['username'] = 'jingcleo';
+$db['demo']['password'] = 'pancit1983';
+$db['demo']['database'] = 'jingcleo_ragnagear';
+$db['demo']['dbdriver'] = 'mysql';
+$db['demo']['dbprefix'] = '';
+$db['demo']['pconnect'] = FALSE;
+$db['demo']['db_debug'] = TRUE;
+$db['demo']['cache_on'] = FALSE;
+$db['demo']['cachedir'] = '';
+$db['demo']['char_set'] = 'utf8';
+$db['demo']['dbcollat'] = 'utf8_general_ci';
+$db['demo']['swap_pre'] = '';
+$db['demo']['autoinit'] = TRUE;
+$db['demo']['stricton'] = FALSE;
+
 $db['server']['hostname'] = 'localhost';
 $db['server']['username'] = 'root';
 $db['server']['password'] = '1234';
