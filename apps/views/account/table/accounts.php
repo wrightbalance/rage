946e3458ace214eb586dfbaf2efc00462d3414f1
Jinggo Villamor <jingcleovil@gmail.com>
2012-06-29 00:43:03 +0800

    Not show account delai;s

--- apps/views/account/table/accounts.php
+++ apps/views/account/table/accounts.php
@@ -23,8 +23,8 @@
                 "id" => $row['account_id'],
                 "cell" => array(
                 	 "<a href=\"javascript:;\" class=\"view\" data-aid=\"".$row['account_id']."\">".$row['account_id']."</a>"
-                	,$row['userid']
-                	,$row['email']
+                	,'--- DEMO DOESNT ALLOWED TO SHOW THIS ---'//$row['userid']
+                	,'--- DEMO DOESNT ALLOWED TO SHOW THIS ---'//$row['email']
                 	,$group_id
                 	,$row['last_ip']
                 	,$row['lastlogin'] != "0000-00-00 00:00:00" ? date('M d, Y',strtotime($row['lastlogin'])) : 'Never'
